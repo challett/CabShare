a427c6b7fa8d9a4259d3498d7e2491901bf67cc0
Connor Hallett <connor.hallett@gmail.com>
2014-12-02 23:36:56 -0500

    Edituserpage working

--- app/src/main/java/com/example/connor/cabshare/EditProfilePage.java
+++ app/src/main/java/com/example/connor/cabshare/EditProfilePage.java
@@ -1,17 +1,37 @@
 package com.example.connor.cabshare;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
-public class EditProfilePage extends Activity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class EditProfilePage extends ActionBarActivity {
+
+    private AuthData authData;
+    private String newName;
+    private EditText name;
+    private Button submit;
+
+    private Firebase ref = new Firebase("https://intense-torch-3362.firebaseio.com/");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile_page);
+
+        authData = com.example.connor.cabshare.MyActivity.getInstance();
     }
 
 
@@ -32,5 +52,17 @@ public class EditProfilePage extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void submitChanges(View v){
+        submit = (Button)findViewById(R.id.submitButton);
+        name = (EditText)findViewById(R.id.newName);
+        newName = name.getText().toString();
+
+        Map<String, String> newChanges = new HashMap<String, String>();
+        newChanges.put("Name", newName);
+
+        ref.child("users").child(authData.getUid()).setValue(newChanges);
+
     }
 }
